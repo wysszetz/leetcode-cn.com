3446b4af562afc5bafc5ecfd92a5d0671141dc27
wysszetz <wysszetz@163.com>
2020-06-09 11:34:33 +0800

    【新增】面试题46  把数字翻译成字符串

---
+++ 46.interview.translateNum/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+)
+
+func translateNum(num int) int {
+	src := strconv.Itoa(num)
+	p, q, r := 0, 0, 1
+	for i := 0; i < len(src); i++ {
+		p, q, r = q, r, 0
+		r += q
+		if i == 0 {
+			continue
+		}
+		pre := src[i-1 : i+1]
+		if pre <= "25" && pre >= "10" {
+			r += p
+		}
+	}
+	return r
+}
+func main() {
+	num := 12258
+	res := translateNum(num)
+	fmt.Println(res)
+}
